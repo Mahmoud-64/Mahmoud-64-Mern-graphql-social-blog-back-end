a941aab9a34ece99da74ed38c33fe44f2c22e1fc
mahmoud <mahmoudyoussef070@gmail.com>
2021-01-26 17:23:42 +0200

    first commit

---
+++ config.js
@@ -0,0 +1,11 @@
+// module.exports = {
+//   MONGODB:
+//     'mongodb+srv://classsed:6lxxxlexaTslHLPe@cluster0-pcsru.mongodb.net/merng?retryWrites=true',
+//   SECRET_KEY: 'some very secret key'
+// };
+
+module.exports = {
+  MONGODB:
+    "mongodb+srv://mahmoud2:00000000@cluster0.j4ubq.mongodb.net/graphqlMern9879?retryWrites=true&w=majority",
+  SECRET_KEY: "some veryy secret key",
+};
